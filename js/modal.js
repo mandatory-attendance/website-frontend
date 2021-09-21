b4ab1ddfcba679fe6bc8561546e33958af0420bb
Abi-B-28 <abigailborromeo@outlook.com>
2021-09-21 19:01:58 +1000

    Create modal.js
    -abi

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+//Modal Popup for Form Submition Confirmation 
+
+//Gets modal, Disclaimer button and Exit span
+var modal = document.getElementById("Disclaimer");
+var DisclaimerBlock = document.getElementById("DisclaimerBlock")
+var span = document.getElementsByClassName("exit")[0];
+//Opens modal when user clicks on Disclaimer button.
+DisclaimerBlock.onclick = function() {
+    modal.style.display = "block";
+}
+//Closes modal when close button is clicked.
+span.onclick = function() {
+    modal.style.display = "none"
+}
