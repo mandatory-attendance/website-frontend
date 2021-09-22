7b354bcd5c1d346715ea14b97b8140c571d1d66b
Yashik Anand <yashik.anand99@gmail.com>
2021-09-23 03:33:11 +1000

    Footer complete

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+  
