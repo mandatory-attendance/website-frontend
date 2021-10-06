31230d4708793958405c47663a829eb35e2ed1d0
mattbezett <mojbez1@gmail.com>
2021-10-06 16:23:36 +1000

    translation page fix

--- js/translate.js
+++ js/translate.js
@@ -1,25 +1,25 @@
-var count = 1;
-
 function set_count() {
 	count=1;
 }
+
 function get_count() {
 	return count;
 }
+
 function inc_count() {
 	count = count + 1;
 }
 
+set_count();
+
 function iterateRecords(data) {
 
 	console.log(data);
-
 	$.each(data.result.records, function(recordKey, recordValue) {
 
 		var recordEnglish = recordValue["English"];
 		var recordKala = recordValue["Kala Lagaw Ya"];
 		var recordID = recordValue["_id"];
-		// var count = 10;
 
 
 		if(recordEnglish && recordID == get_count()) {
@@ -43,15 +43,12 @@ function iterateRecords(data) {
 document.getElementById("next_button").onclick = function() {next()};
 
 function next() {
-	inc_count();
 	function iterateRecords(data) {
 		$.each(data.result.records, function(recordKey, recordValue) {
 	
 			var recordEnglish = recordValue["English"];
 			var recordKala = recordValue["Kala Lagaw Ya"];
 			var recordID = recordValue["_id"];
-			var count = 10;
-	
 	
 			if(recordEnglish && recordID == get_count()) {
 				$("#records").append(
@@ -70,6 +67,7 @@ function next() {
 			}
 		});
 	console.log(count);
+	inc_count()
 }
 
 $(document).ready(function() {
@@ -77,8 +75,6 @@ $(document).ready(function() {
 	var data = {
 		resource_id: "9229d441-bdcc-40a9-8ad9-d287b2d679c4"
 	}
-
-	set_count();
 
 	$.ajax({
 		url: "https://www.data.qld.gov.au/api/3/action/datastore_search",
