b68566ad103e22ae9e09eb1c6b2065b90816c21c
Yashik Anand <yashik.anand99@gmail.com>
2021-10-11 11:33:03 +1000

    Fixed disclaimer function in php

--- php/disclaimer.php
+++ php/disclaimer.php
@@ -1,15 +1,17 @@
 <nav class="button">
-            <a id="DisclaimerBlock">DISCLAIMER</a>
-        </nav>
+    <a id="DisclaimerBlock">DISCLAIMER</a>
+</nav>
     
-        <div id="Disclaimer" class="modal">
-    
-            <div class="DisclaimerContent">
-                <!--<span class="exit">&times;</span> -->
-                <h1>Disclaimer</h1>
-                <p>Aboriginal and Torres Strait Islander viewers are advised that this website may contain images of deceased persons.<br>
-                    Users are warned that there may be words and descriptions that may be culturally sensitive and which might not normally be used in certain public or community contexts.<br>
-                    Terms and annotations that reflect the attitude of the author or the period in which the item was written, may be considered inappropriate today.</p>
-                <span class="Continuebutton"><button class="ContinueButton">Continue</button></span>
-            </div>
-        </div>
+<div id="Disclaimer" class="modal">
+
+<div class="DisclaimerContent">
+    <!--<span class="exit">&times;</span> -->
+    <h1>Disclaimer</h1>
+    <p>Aboriginal and Torres Strait Islander viewers are advised that this website may contain images of deceased persons.<br>
+        Users are warned that there may be words and descriptions that may be culturally sensitive and which might not normally be used in certain public or community contexts.<br>
+        Terms and annotations that reflect the attitude of the author or the period in which the item was written, may be considered inappropriate today.</p>
+    <span class="Continuebutton"><button class="ContinueButton">Continue</button></span>
+</div>
+</div>
+
+<script src="../js/disclaimer.js"></script>
