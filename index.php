8b2c0d0cb5b6e0d716e4b6a5079417b239730b20
Abi-B-28 <abigailborromeo@outlook.com>
2021-10-11 13:12:41 +1000

    index php

---
+++ index.php
@@ -0,0 +1,115 @@
+<head>
+        <meta charset="utf-8">
+        <title>Homepage</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- <div class="disclaimer">
+            <a href="#" id="disclaimer-link">Disclaimer</a>
+        </div> -->
+
+        <?php include 'disclaimer.php'; ?>
+
+        <div id="page-header">
+            <div class="klylogo">
+                <a href="index.html">
+                <img src="images/KLY LOGO.png" alt="KLY LOGO">
+                </a>
+            </div>
+
+            <div class="options">
+                <figure>
+                    <a href="html/about.html">
+                    <img src ="images/about.png"  alt="kathy">
+                    </a>
+                    <figcaption>ABOUT</figcaption>
+                </figure>
+                <figure>
+                    <a href="html/flash.html">
+                    <img src ="images/flash.png"  alt="steve">
+                    </a>
+                    <figcaption>FLASHCARD</figcaption>
+                </figure>
+                <figure>
+                    <a href="html/translation.html">
+                    <img src ="images/translate.png" alt="todd">
+                    </a>
+                    <figcaption>TRANSLATE</figcaption>
+                </figure>
+                <figure>
+                    <a href="html/puzzle.html">
+                    <img src ="images/puzzle.png" alt="don">
+                    </a>
+                    <figcaption>PUZZLE</figcaption>
+                </figure>
+                <figure>
+                    <a href="index.html">
+                    <img src ="images/quiz.png" alt="sean">
+                    </a>
+                    <figcaption>QUIZ</figcaption>
+                </figure>
+                </div>
+
+<!--             <div class="options">
+                <<img src="images/home.png" alt="home icon"> 
+                <img src="images/about.png" alt="about icon">
+                <p>about</p>
+                <img src="images/flash.png" alt="flash icon">
+                <p>flash card</p>
+                <img src="images/translate.png" alt="translate icon">
+                <p>translate</p>
+                <img src="images/puzzle.png" alt="puzzle icon">
+                <p>puzzel</p>
+                <img src="images/quiz.png" alt="quiz icon">
+                <p>quiz</p>
+            </div> -->
+        </div>
+
+        <article>
+            
+            <img src="images/flag.jpeg" alt="Torres Strait Islander Flag" style="width:25%">
+    
+                <p>    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco 
+                </p>
+
+        </article>
+
+        <div class="modes-button">
+            <div class="learning-mode-button">
+                <a href="html/learning.html">FLASHCARD MODE</a>
+            </div>
+            <div class="translation-mode-button">
+                <a href="html/translation.html">TRANSLATION MODE</a>
+            </div>
+            <div class="puzzle-mode-button">
+                <a href="html/#">PUZZLE MODE</a>
+            </div>
+        </div>
+
+        <div class="references">
+            <h1>References</h1>
+
+        </div>
+
+    </body>
