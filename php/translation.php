9833dd219da22bdb751fb132b9af04b2ab501c44
Yashik Anand <yashik.anand99@gmail.com>
2021-10-11 11:31:07 +1000

    Created disclaimer php and working on translation page

---
+++ php/translation.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Translation</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <?php include 'disclaimer.php';?>
+</body>
+</html>
