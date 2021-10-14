7d14daf2356487a5a7133ea16e90d197bba5debf
Yashik Anand <yashik.anand99@gmail.com>
2021-10-15 09:51:13 +1000

    File arrangement and structured folders for cleaner code

---
+++ php/support/head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+</head>
