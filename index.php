14f3a544ec4578a09166b2a689ebd30a384ae623
nishavashist <89570829+nishavashist@users.noreply.github.com>
2021-10-16 01:33:04 +1000

    newslideshow test

--- index.php
+++ index.php
@@ -10,100 +10,13 @@
     <body id="banner">
         <?php include './php/support/homepage/disclaimer.php';?>
         <?php include './testnisha/test-fullsize.php';?>
-
-        <div class="slideshow">
-            <div class="slideshow-item">
-                <img src="./../images/1.jpeg" alt="">
-                <div class="slideshow-item-text">
-                    <h5>sup </h5>
-                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
-                </div>
-            </div>  
-
-            <div class="slideshow-item">
-                <img src="./../images/2.jpeg" alt="">
-                <div class="slideshow-item-text">
-                    <h5>bye </h5>
-                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
-                </div>
-            </div>  
-
-            <div class="slideshow-item">
-                <img src="./../images/3.png" alt="">
-                <div class="slideshow-item-text">
-                    <h5>okay </h5>
-                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
-                </div>
-            </div>  
-        </div>
-
-       <!--  <div class="slideshow"></div> -->
+        <?php include './testnisha/testslideshow.php';?>
 
 
 
 
-        <div class="modes-button">
-            <div class="learning-mode-button">
-                <a href="php/flashcard.php">FLASHCARD MODE</a>
-            </div>
-            <div class="translation-mode-button">
-                <a href="php/translation.php">TRANSLATION MODE</a>
-            </div>
-            <div class="puzzle-mode-button">
-                <a href="php/puzzle.php">PUZZLE MODE</a>
-            </div>
-        </div>
     </body>
 </html>
 
                 
 
-
-
-
-
-<!--             <div class="options">
-                <<img src="images/home.png" alt="home icon"> 
-                <img src="images/about.png" alt="about icon">
-@ -104,25 +99,36 @@
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco 
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                    quis nostrud exercitation ullamco 
-                </p>
-
-        </article>
-
-        <div class="modes-button">
-            <div class="learning-mode-button">
-                    <a href="html/learning.html">Learning Mode</a>
-                <a href="html/learning.html">FLASHCARD MODE</a>
-            </div>
-            <div class="translation-mode-button">
-                    <a href="html/translation.html">Translation Mode</a>
-                <a href="html/translation.html">TRANSLATION MODE</a>
-            </div>
-            <div class="puzzle-mode-button">
-                    <a href="html/#">Puzzle Mode</a>
-                <a href="html/#">PUZZLE MODE</a>
-            </div>
-        </div>
-
-        </article>
-
-        <div class="references">
-            <h1>References</h1>
-
-        </div>
-
-    </body>
-</html>
---
+++ testnisha/testslideshow.php
@@ -0,0 +1,26 @@
+<div class="slideshow">
+            <div class="slideshow-item">
+                <img src="../images/flash.png" alt="hello">
+                <div class="slideshow-item-text">
+                    <h5>sup </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>  
+
+            <div class="slideshow-item">
+                <img src="../images/puzzle.png" alt="seeya">
+                <div class="slideshow-item-text">
+                    <h5>bye </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>  
+
+            <div class="slideshow-item">
+                <img src="../images/quiz.png" alt="note">
+                <div class="slideshow-item-text">
+                    <h5>okay </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>  
+    
+</div>
