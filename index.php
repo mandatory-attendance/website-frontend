f1918212de6cd511a97b310eddf7088b96d9504a
nishavashist <89570829+nishavashist@users.noreply.github.com>
2021-10-17 00:57:31 +1000

    slideshow

--- index.php
+++ index.php
@@ -10,11 +10,51 @@
     <body id="banner">
         <?php include './php/support/homepage/disclaimer.php';?>
         <?php include './testnisha/test-fullsize.php';?>
-        <?php include './testnisha/testslideshow.php';?>
+        
+        <script src="./js/slideshow.js"></script>
+        <div class="slideshow-container">
 
+            <div class="mySlides">
+                <div class="numbertext">1 / 3</div>
+                <img src="./images/map.jpg">
+                <div class="slideshow-item-text">
+                    <h5>okay </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>
 
+            <div class="mySlides">
+                <div class="numbertext">2 / 3</div>
+                <img src="./images/2.png">
+                <div class="slideshow-item-text">
+                    <h5>okay </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>
 
+            <div class="mySlides">
+                <div class="numbertext">3 / 3</div>
+                <img src="./images/1.png">
+                <div class="slideshow-item-text">
+                    <h5>okay </h5>
+                    <p>hedsjhuhfdhjhhdhfuhdfuhdfuhduhduhudsfhbdhbdhbd</p>
+                </div>
+            </div>
 
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+        </div>
+
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot" onclick="currentSlide(1)"></span> 
+            <span class="dot" onclick="currentSlide(2)"></span> 
+            <span class="dot" onclick="currentSlide(3)"></span> 
+        </div>
+
+             
     </body>
 </html>
 
