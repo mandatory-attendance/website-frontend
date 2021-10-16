e8b40d9cd5d32fb8e7e81fc535ab3f5548384cc5
Abi-B-28 <abigailborromeo@outlook.com>
2021-10-16 18:54:28 +1000

    about tets

---
+++ test Abi/test.about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <?php include '..//php/support/head.php';?>
+        <title>Homepage</title>
+        <link rel="stylesheet" href="..//css/style.css">
+    </head>
+
+    <?php include '..//php/support/disclaimer.php';?>
+
+    <body id="banner">
+        <article id="test-about-contents">
+         
+            <div class="about-row">
+                <section class="about-description">
+                    <h1>Kalaw Lagaw Ya Language</h1>
+                    <p>
+                        Kala Lagaw Ya is one of two traditional languages of the Torres Strait. Kala Lagaw Ya (also written as Kalaw Lagaw Ya) 
+                        is the traditional language owned by the Western and Central islands of the Torres Strait. It is linguistically connected 
+                        to the Aboriginal languages of the Australian mainland.
+                    </p>
+                </section>
+                <section class="about-image">
+                    <img src="./../images/islands.JPG">
+                </section>
+            </div>
+
+            <div class="about-row">
+                <section style="text-align:center">
+                    <iframe width="580" height="350" src="https://www.youtube.com/embed/UD-xWwNtPXI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </section>
+                <section class="about-description">
+                    <h1></h1>
+                    <p>
+                        Kala Lagaw Ya is one of two traditional languages of the Torres Strait. Kala Lagaw Ya (also written as Kalaw Lagaw Ya) 
+                        is the traditional language owned by the Western and Central islands of the Torres Strait. It is linguistically connected 
+                        to the Aboriginal languages of the Australian mainland.
+                    </p>
+                </section>
+            </div>
+
+        </article>
+
+    </body>
+
+</html>
