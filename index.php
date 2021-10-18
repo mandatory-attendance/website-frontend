92c7eee913f5424c820e22691e9fc829b476b0a8
nishavashist <89570829+nishavashist@users.noreply.github.com>
2021-10-19 01:48:25 +1000

    header stick to page, changed disclamier background and added the two icons in

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
         <script src="./js/slideshow.js"></script>
         <div class="slideshow-container">
 
+        <img class="ruler" src="./images/ruler.png">
+        <img  class="pencil"src="./images/pencil.png">
+
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
                 <img src="./images/map.jpg">
@@ -82,8 +85,32 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 </script>
+
+<script>
+var navbar; 
+var offset; 
+
+// Add the offset class to the navbar when the user reaches its scroll position. Remove "sticky" when the user leave the scroll position
+function myscroll() {
+  if (window.pageYOffset >= offset) { // If user moves from the top of page relocates the navbar
+    navbar.classList.add("offset")
+  } else {
+    navbar.classList.remove("offset"); 
+  }
+}
+
+//when the user scrolls down the page function is executed 
+window.onscroll = function() {myscroll()};
+
+window.onload = function() {
+  navbar = document.getElementById("page-header"); //gets the navbar
+  offset = navbar.offsetTop; //gets the offset position of the navbar
+  myscroll();
+}; 
     </body>
 </html>
+
+
 
                 
 
