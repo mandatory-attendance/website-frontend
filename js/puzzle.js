df9ecc51ced525ec2748693626c50ad36e149fae
mattbezett <mojbez1@gmail.com>
2021-10-19 14:06:13 +1000

    puzzle image fix

--- js/puzzle.js
+++ js/puzzle.js
@@ -39,10 +39,10 @@ function iterateRecords(data) {
 			);
       
       $("#puzzleQuestion").append(
-				$('<img id = "picture" src = ../../images/'+ currentWordId + '.png alt = "' + recordEnglish +'">')
+				$('<img id = "picture" src = "../images/'+ currentWordId + '.png" alt = "' + recordEnglish +'">')
 			);
       // $('#puzzleQuestion').append('<img src="../images/' + recordID + '.png" />'); 
-      
+
       //Setting up the answer and the choice
 
       let kalaLetter = recordKala.toUpperCase();
