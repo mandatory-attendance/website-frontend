0d0aa83d4e9be15c682f40c182c2e1008b944d26
mattbezett <mojbez1@gmail.com>
2021-10-20 13:31:17 +1000

    fixed links

--- php/support/interactionheader.php
+++ php/support/interactionheader.php
@@ -1,37 +1,37 @@
 <div id="interactHeader">
     <div id="interactLogo">
-        <a href="/index.php">
+        <a href="index.php">
         <img src="./../images/KLY LOGO.png" alt="KLY LOGO">
         </a>
     </div>
 
     <div id="interactIcon">
         <figure>
-            <a href="./php/about.php">
+            <a href="about.php">
             <img src ="./../images/about.png"  alt="kathy">
             </a>
             <figcaption>ABOUT</figcaption>
         </figure>
         <figure>
-            <a href="./php/flashcard.php">
+            <a href="flashcard.php">
             <img src ="./../images/flash.png"  alt="steve">
             </a>
             <figcaption>FLASHCARD</figcaption>
         </figure>
         <figure>
-            <a href="./php/translation.php">
+            <a href="translation.php">
             <img src ="./../images/translate.png" alt="todd">
             </a>
             <figcaption>TRANSLATE</figcaption>
         </figure>
         <figure>
-            <a href="./php/puzzle.php">
+            <a href="puzzle.php">
             <img src ="./../images/puzzle.png" alt="don">
             </a>
             <figcaption>PUZZLE</figcaption>
         </figure>
         <figure>
-            <a href="./php/quiz.php">
+            <a href="quiz.php">
             <img src ="./../images/quiz.png" alt="sean">
             </a>
             <figcaption>QUIZ</figcaption>
