2674b7b44485a62b4c18a5c587242b66389dd78e
mattbezett <mojbez1@gmail.com>
2021-10-23 22:27:55 +1000

    small fixes

--- php/support/homepage/navbar.php
+++ php/support/homepage/navbar.php
@@ -1,38 +1,38 @@
 <div id="page-header">
     <div id="klylogo">
-        <a href="/index.php">
-        <img src="./images/KLY LOGO.png" alt="KLY LOGO">
+        <a href="index.php">
+        <img src="images/KLY LOGO.png" alt="KLY LOGO">
         </a>
     </div>
 
     <div id="options">
         <figure>
-            <a href="./php/about.php">
-            <img src ="./images/about.png"  alt="about icon">
+            <a href="php/about.php">
+            <img src ="images/about.png"  alt="about icon">
             </a>
             <figcaption>ABOUT</figcaption>
         </figure>
         <figure>
-            <a href="./php/flashcard.php">
-            <img src ="./images/flash.png"  alt="flash card icon">
+            <a href="php/flashcard.php">
+            <img src ="images/flash.png"  alt="flash card icon">
             </a>
             <figcaption>FLASHCARD</figcaption>
         </figure>
         <figure>
-            <a href="./php/translation.php">
-            <img src ="./images/translate.png" alt="translation icon">
+            <a href="php/translation.php">
+            <img src ="images/translate.png" alt="translation icon">
             </a>
             <figcaption>TRANSLATE</figcaption>
         </figure>
         <figure>
-            <a href="./php/puzzle.php">
-            <img src ="./images/puzzle.png" alt="puzzle icon">
+            <a href="php/puzzle.php">
+            <img src ="images/puzzle.png" alt="puzzle icon">
             </a>
             <figcaption>PUZZLE</figcaption>
         </figure>
         <figure>
-            <a href="./php/quiz.php">
-            <img src ="./images/quiz.png" alt="quiz icon">
+            <a href="php/quiz.php">
+            <img src ="images/quiz.png" alt="quiz icon">
             </a>
             <figcaption>QUIZ</figcaption>
         </figure>
